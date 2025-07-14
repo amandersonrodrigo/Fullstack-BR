bc306f92c6ce39e3a136ca38b0b5565c6f7123f0
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2025-07-14 12:53:42 -0300

    atualizado

---
+++ aula6/index2.js
@@ -0,0 +1,21 @@
+let nome; // Declarou variável
+nome = 'Amanderson'; // Inicializando Variável
+console.log(nome); 
+nome= 'Rodrigo';
+console.log(nome);
+
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+
+let nomeCliente = 'Levi';
+nomeCliente = 'Rodrigo';
+console.log(nomeCliente);
+
+// Não pode começar o nome de uma variável com número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// let nomeCliente = 'Amanderson';
+// let nomecliente = 'Rodrigo';
+// Não utilizar Var, Utilizar let.
