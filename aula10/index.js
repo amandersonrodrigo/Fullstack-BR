536bb574f79f91e4deee32b707125905325c20c8
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2025-07-15 02:30:46 -0300

    atualizando

---
+++ aula10/index.js
@@ -0,0 +1,19 @@
+ /*  Operadores aritméticos 
+ + Adição  / Concatenação = unir 2 valores
+ - / * 
+ ** Potenciação
+ % Resto da divisão
+
+ Ordem de Precedencia (), **, * / %, + -
+ */
+const num1 = 10;
+const num2 = 3;
+console.log(num1 % num2);
+
+
+//Operadores de atribuição
+let contador = 2;
+contador *= 2; // contador recebe contador * 2 
+contador *= 2;
+contador *= 2;
+console.log(contador);
