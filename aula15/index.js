5c8fb2e1e9552bbecd5fc15853f3c0ef8594ec99
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2025-07-29 13:33:12 -0300

    atualizando

---
+++ aula15/index.js
@@ -0,0 +1,12 @@
+let num1 = 9;
+// console.log(num1 ** 0.5); pega a raiz quadrada
+
+// let num2 = Math.floor(num1); // arredonda o número para baixo
+// let num2 = Math.ceil(num1); arredonda o número pra cima
+// let num2 = Math.round(num1); da metade pra cima arredonda pra cima
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); vai dar o maior número dessa sequencia
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); vai dar o menor número dessa sequencia
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5); da um numero aleatorio entre 5 e 10
+
+// console.log(Math.PI);
+// console.log(Math.pow(2, 5)); = 2 ** 5
