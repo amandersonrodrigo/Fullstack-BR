09453937a71c95d803a1446270098e9ca70a299f
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2025-08-04 01:52:39 -0300

    atualizando

--- aula15/ex/js/script.js
+++ aula15/ex/js/script.js
@@ -1,3 +1,2 @@
-let num = prompt(`Digite um número: `);
-console.log(`<h1>Seu número é ${num}</h1><br>`);
-console.log(`Raiz quadrada: ${num ** 0.5} <br>`);
+const num = prompt('Digite um número:');
+const numeroTitulo = document.getElementById('numero-titulo');
