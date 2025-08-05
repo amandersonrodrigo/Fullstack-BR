f82e90c28755f118c10583b6c6e7d647a6c6646b
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2025-08-05 13:26:08 -0300

    atualizando

--- aula15/ex/js/script.js
+++ aula15/ex/js/script.js
@@ -2,5 +2,5 @@ const numero = Number(prompt('Digite um número:'));
 const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
-numeroTitulo.innerHTML = numeroTitulo;
-texto.innerHTML = `<p>Seu Número + 2 é: ${numero}</p>`
+numeroTitulo.innerHTML = numero;
+texto.innerHTML = `<p>Seu Número + 2 é: ${numero + 2}</p>`
