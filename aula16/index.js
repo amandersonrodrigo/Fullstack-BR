70153ec258980088bece759f9046318474b40339
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2025-09-09 02:58:36 -0300

    ATUALIZANDO

---
+++ aula16/index.js
@@ -0,0 +1,11 @@
+//
+const alunos = ['Luiz', 'Maria', 'João'];
+
+console.log(alunos.length);
+
+// alunos[0] = 'Eduardo';
+// alunos[3] = 'Rodrigo';
+
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2]);
