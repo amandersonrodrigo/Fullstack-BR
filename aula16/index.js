ce9d4b47faa38e9df076e011e6b6d523fed07e8d
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2025-09-11 00:59:06 -0300

    atualizando

--- aula16/index.js
+++ aula16/index.js
@@ -1,10 +1,31 @@
-//
-const alunos = ['Luiz', 'Maria', 'João'];
+//                0       1        2          3       4
+const alunos = ['Luiz', 'Maria', 'João']; // Luiza, Eduardo
 
-console.log(alunos.length);
+console.log(typeof alunos); // retorna objeto, array é considerado um objeto
+console.log(alunos instanceof Array); // retorna verdadeiro. Pergunta se a variavel é um array
 
-// alunos[0] = 'Eduardo';
-// alunos[3] = 'Rodrigo';
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+
+// console.log(alunos.slice(0, 3)); // fatia o array
+
+// console.log(alunos[50]);
+
+
+// delete alunos[1]; // deleta o item e deixa o índice vazio
+// console.log(alunos[1]);
+
+// const removido = alunos.pop(); // remove o ultimo elemento
+// const removido = alunos.shift(); // remove o primeiro elemento
+// console.log(removido);
+
+// alunos.unshift('Luiza'); // adiciona o elemento no inicio do array
+// alunos.unshift('Eduardo');
+
+// alunos.push('Amanderson'); // adiciona o elemento no final do array
+
+// alunos[0] = 'Eduardo'; adiciona o elemento no índice 0
+// alunos[3] = 'Rodrigo'; 
 
 // console.log(alunos);
 // console.log(alunos[0]);
---
+++ aula17/index.js
@@ -0,0 +1,31 @@
+// const raiz = function (n) {
+const raiz = n => n ** 0.5;    // função moderna
+    
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+console.log(raiz(36));
+
+
+// function soma(x = 1, y = 2) {
+//     const resultado = x + y;
+//     return resultado;
+// }
+
+// const resultado = soma(3, 7);
+// console.log(resultado);
+
+
+// console.log(soma(2, 2));
+// console.log(soma(10, 2));
+// console.log(soma(5, 10));
+
+// function saudacao(nome) {
+//     return `Bom dia ${nome}!`;
+// }
+
+// const variavel = saudacao('Rodrigo');
+// console.log(variavel);
+
+// // saudacao('Rodrigo');
+// // saudacao('Laryssa');
