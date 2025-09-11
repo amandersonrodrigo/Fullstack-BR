07d316bef1bd9d441e37078db4d8bf2aba189c81
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2025-09-11 13:23:55 -0300

    atualizando

---
+++ aula19/index.js
@@ -0,0 +1,7 @@
+/* 
+Primitivos (imutáveis) - string, number, boolean, undefined, null (bigint, symbol)
+*/
+//          0123456
+let nome = 'Rodrigo';
+nome[0] = 'L'; // nao altera em nada pq é um dado imutável
+console.log(nome[0], nome);
