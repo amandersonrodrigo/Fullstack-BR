95cc3172d8f3caa6208eed1e0c4c8b2f3f3e272c
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2025-09-26 13:36:41 -0300

    atualizando

---
+++ aula21/index.js
@@ -0,0 +1,15 @@
+/* 
+Operadores de comparação
+> Maior que
+>= Maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (checa valor)
+=== igualdade estrita (checa valor e tipo)
+!= diferente (checa valor)
+!== diferente estrito (checa valor e tipo)
+*/
+const num1 = 5;
+const num2 = 5;
+const comp = num1 !== num2;
+console.log(comp);
---
+++ aula22/index.js
@@ -0,0 +1,10 @@
+/*
+Operadores lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU -> Se apenas uma das expressões ser verdadeira, irá retornar true
+! -> NOT -> NÃO 
+*/
+// const expressaoAnd = true && true && true && true;
+const expressaOr = true || false;
+console.log(expressaOr);
+
