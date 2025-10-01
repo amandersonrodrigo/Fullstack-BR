b84d0d38c03110ff84324e81748031ab8daa723a
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2025-10-01 03:14:22 -0300

    atualiznado

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -5,3 +5,8 @@ form.addEventListener('submit', function (evento) {
     evento.preventDefault();
     console.log('Evento previnido');
 });
+
+function setResultado (msg) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = msg;
+}
