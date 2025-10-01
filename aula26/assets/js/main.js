12344b42a0d36b155c6b3c8151bf3b8acf9e2f8e
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2025-10-01 13:44:25 -0300

    atualizando

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -3,10 +3,26 @@ const form = document.querySelector('#formulario');
 
 form.addEventListener('submit', function (evento) {
     evento.preventDefault();
-    console.log('Evento previnido');
+    const inputPeso = evento.target.querySelector('#peso');
+    const inputAltura = evento.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso) {
+        setResultado('Peso inválido');
+        return;
+    }
 });
+
+function criaP () {
+    const p = document.createElement('p');
+    return p;
+
+}
 
 function setResultado (msg) {
     const resultado = document.querySelector('#resultado');
-    resultado.innerHTML = msg;
+    resultado.innerHTML = '';
+    const p = criaP();
 }
